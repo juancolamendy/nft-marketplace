adee8593971f03eeda8eaccad03639103c2cf676
Juan Carlos Olamendy <juanc.olamendy@gmail.com>
2021-11-29 10:04:02 -0500

    add deployment to ropsten

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
 
-const privateKey = process.env.PRIVATE_KEY;
-const infuraId = process.env.INFURA_ID;
-
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -12,8 +9,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 });
 
 task("envs", "Prints the list of environment variables", async () => {
-	console.log('Private Key:', privateKey);
-	console.log('Infura Id:', infuraId);
+  console.log('INFURA_KEY:', process.env.INFURA_KEY);
+  console.log(`ROPSTEN_ACCT_SECRET: 0x${process.env.ROPSTEN_ACCT_SECRET}`);
 });
 
 module.exports = {
@@ -22,6 +19,10 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [`0x${process.env.ROPSTEN_ACCT_SECRET}`]
+    }    
   },
   solidity: {
     version: "0.8.4",
