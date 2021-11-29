13e4944c486c06051c65e640bf95df02e020959f
Juan Carlos Olamendy <juanc.olamendy@gmail.com>
2021-11-28 21:26:13 -0500

    buyers assets

--- pages/my-assets.js
+++ pages/my-assets.js
@@ -7,14 +7,12 @@ import {
   nftmarketaddress, nftaddress
 } from '../config'
 
-// import ABI to interact with contract on the client-side
 import NFT from '../artifacts/contracts/NFT.sol/NFT.json';
 import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json';
 
-export default function CreatorDashboard() {
+export default function MyAssets() {
   // state management
   const [nfts, setNfts] = useState([])
-  const [sold, setSold] = useState([])
   const [loadingState, setLoadingState] = useState('not-loaded')
 
   // effects
@@ -22,6 +20,7 @@ export default function CreatorDashboard() {
     loadNFTs()
   }, [])
 
+  // functions
   async function loadNFTs() {
     // open modal to get a connection
     const web3Modal = new Web3Modal()
@@ -30,13 +29,13 @@ export default function CreatorDashboard() {
     const provider = new ethers.providers.Web3Provider(connection)
     // get signer
     const signer = provider.getSigner()
-
+      
     // reference contracts
     const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
     const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
 
-    // call fetchItemsCreated in marketContract
-    const data = await marketContract.fetchItemsCreated()
+    // call fetchMyNFTs in marketContract
+    const data = await marketContract.fetchMyNFTs()
     
     const items = await Promise.all(data.map(async i => {
       const tokenUri = await tokenContract.tokenURI(i.tokenId)
@@ -47,29 +46,23 @@ export default function CreatorDashboard() {
         tokenId: i.tokenId.toNumber(),
         seller: i.seller,
         owner: i.owner,
-        sold: i.sold,
         image: meta.data.image,
       }
       return item
     }))
     
-    // filter out array of items that have been sold
-    const soldItems = items.filter(i => i.sold)
-    
     // update the state
-    setSold(soldItems)
     setNfts(items)
     setLoadingState('loaded') 
   }
 
-  // render out the view
-  if (loadingState === 'loaded' && !nfts.length) return (<h1 className="py-10 px-20 text-3xl">No assets created</h1>)
-  
+  // render out view
+  if (loadingState === 'loaded' && !nfts.length) return (<h1 className="py-10 px-20 text-3xl">No assets owned</h1>)
+
   return (
-    <div>
+    <div className="flex justify-center">
       <div className="p-4">
-        <h2 className="text-2xl py-2">Items Created</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
           {
             nfts.map((nft, i) => (
               <div key={i} className="border shadow rounded-xl overflow-hidden">
@@ -82,29 +75,6 @@ export default function CreatorDashboard() {
           }
         </div>
       </div>
-
-      <div className="px-4">
-      {
-        Boolean(sold.length) && (
-          <div>
-            <h2 className="text-2xl py-2">Items sold</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-              {
-                sold.map((nft, i) => (
-                  <div key={i} className="border shadow rounded-xl overflow-hidden">
-                    <img src={nft.image} className="rounded" />
-                    <div className="p-4 bg-black">
-                      <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
-                    </div>
-                  </div>
-                ))
-              }
-            </div>
-          </div>
-        )
-      }
-      </div>
-
     </div>
   )
 }
